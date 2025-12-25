6eaccda6a0443ba72f57d91708ae76cce870ae0b
soutori296 <soutori296@gmail.com>
2025-12-25 23:28:58 +0900

    Update app_free.py

--- app_free.py
+++ app_free.py
@@ -336,6 +336,7 @@ with st.sidebar:
         reload_button_clicked = col_reload.button("🔄再診", on_click=reanalyze_all_data_logic, use_container_width=True, disabled=is_reload_disabled)
         
         if st.session_state.is_running_continuous:
+             st.markdown("---")
              if st.button("🛑分析中止", use_container_width=True, key='cancel_continuous_key_large'):
                  st.session_state.is_running_continuous = False
                  st.session_state.wait_start_time = None
@@ -905,7 +906,7 @@ def calculate_score_and_logic(df, info, vol_ratio, status):
 
     # 市場全体リスク
     if get_25day_ratio() >= 125.0:
-        score -= 20; factors["市場過熱"] = -20
+        score -= 10; factors["市場過熱"] = -10
 
     # テクニカル・出来高
     if is_gc: score += 5; factors["GC発生"] = 5
@@ -1644,4 +1645,3 @@ if st.session_state.analyzed_data:
         実際の投資判断や売買に用いることを目的としていません。
     </div>
     """, unsafe_allow_html=True)
-
